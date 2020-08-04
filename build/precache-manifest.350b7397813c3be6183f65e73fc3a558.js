6ab7196c1818a0825930aeda6ab07f2e3a80bdb1
Mahmood Khan <sk77.mobilefixu@gmail.com>
2020-08-04 23:33:24 +0530

    Version -1.1 | Minor bug fixes

---
+++ build/precache-manifest.350b7397813c3be6183f65e73fc3a558.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f99bcbf7dc136c9521a341e8cad5f04",
+    "url": "/react-contact-app/index.html"
+  },
+  {
+    "revision": "39060614b581c1e34ecc",
+    "url": "/react-contact-app/static/js/2.2d80bb48.chunk.js"
+  },
+  {
+    "revision": "b409c09d6956996dc16f",
+    "url": "/react-contact-app/static/js/main.5f8559b8.chunk.js"
+  },
+  {
+    "revision": "c94d0704127985839515",
+    "url": "/react-contact-app/static/js/runtime-main.081ef2b1.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./component/App.js";
+
+ReactDOM.render (<App />, document.getElementById("root"));
